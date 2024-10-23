f1949e2173258b171c7d57fe8002156bc1d10f79
aqua <aqualauwers63@gmail.com>
2024-10-23 10:38:27 +0200

    made the button wiggle

--- cute chatbox/scripts/js/login.js
+++ cute chatbox/scripts/js/login.js
@@ -4,11 +4,26 @@ function login() {
 
     var usernameInput = document.getElementById("User").value;
     var passwordInput = document.getElementById("Password").value;
+    var User = document.getElementById("User");
+    var Password = document.getElementById("Password");
 
     if (usernameInput === usernameCorrect && passwordInput === passwordCorrect) {
         window.location.replace("https://aquafoxy9182.github.io/cute%20chatbox/scripts/html/chat.html");
     } else {
         document.getElementById("User").value = "";
         document.getElementById("Password").value = "";
+        User.classList.add("UserPasswordAnimate");
+        Password.classList.add("UserPasswordAnimate");
+
+        // < van het internet >
+        User.classList.remove('UserPasswordAnimate');
+        void User.offsetWidth;  // This forces reflow
+        User.classList.add('UserPasswordAnimate');
+
+        Password.classList.remove('UserPasswordAnimate');
+        void Password.offsetWidth;  // This forces reflow
+        Password.classList.add('UserPasswordAnimate');
+        // < /van het inernet >
+
     }
 }
